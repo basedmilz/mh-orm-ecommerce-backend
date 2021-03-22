30550245754d0f17ee2eb8cb41fd7059e54509c5
basedmilz <basedmilz@gmail.com>
2021-03-22 16:47:38 -0400

    modified index html

--- models/Product.js
+++ models/Product.js
@@ -9,11 +9,20 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
     // define columns
     product_name: {type: DataTypes.STRING, allowNull:false},
-    price: {type: DataTypes.NUMBER, allowNull:false},
-    stock: {type: DataTypes.NUMBER, allowNull:false},
-    category_id: {type: DataTypes.STRING, allowNull:false},
+    price: {type: DataTypes.DECIMAL, allowNull:false},
+    stock: {type: DataTypes.INTEGER, allowNull:false},
+    category_id: {type: DataTypes.INTEGER, references:{
+      model:'category', 
+      key:'id',
+    }},
   },
   {
     sequelize,
